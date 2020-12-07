09b996d3a0b8470a71e09a1726687854432b3ede
Wleydson Lemos <wleydsonredes@gmail.com>
2020-12-07 20:42:08 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { TemplateModule } from './template/template.module';
+import { ClientsModule } from './clients/clients.module';
+
+import { HomeComponent } from './home/home.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TemplateModule,
+    ClientsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
